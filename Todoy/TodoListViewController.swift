f49c99852deb103fffc52ee65fb804f61be03120
PatrikCesnek <pcesnek290@gmail.com>
2019-06-16 16:34:10 +0200

    Data presist using UserDefault method.

--- Todoy/TodoListViewController.swift
+++ Todoy/TodoListViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["First item", "Second item", "Third item"]
+    
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
     }
     
     //Mark - Tableview Datasource Methods
@@ -64,6 +70,8 @@ class TodoListViewController: UITableViewController {
             //what will happen once the user clicks on add button
             
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             
             self.tableView.reloadData()
             

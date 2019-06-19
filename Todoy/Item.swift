37c141524b2f5dcb7c311da4c9adb99cb2f7c587
PatrikCesnek <pcesnek290@gmail.com>
2019-06-19 09:22:10 +0200

    Convert to MVC design pattern

---
+++ Todoy/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoy
+//
+//  Created by Patrik Cesnek on 17/06/2019.
+//  Copyright © 2019 Patrik Cesnek. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title = ""
+    
+    var done = false
+}
